bccbe5f6f4121274bc0e64dc3230f780276221bd
Chow Loong Jin <hyperair@debian.org>
2014-01-14 12:25:29 +0800

    Add support for rendering with a URL prefix
    This adds an optional MEDUSA_URL_PREFIX setting option that causes Django's URL
    reversing to render URLS prefixed with this string. This is necessary when
    hosting Django projects on a URI path other than /, as a proper WSGI environment
    is not present to tell Django what URL prefix to use.

--- django_medusa/management/commands/staticsitegen.py
+++ django_medusa/management/commands/staticsitegen.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.core.urlresolvers import set_script_prefix
 from django_medusa.renderers import StaticSiteRenderer
 from django_medusa.utils import get_static_renderers
 
@@ -11,6 +13,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         StaticSiteRenderer.initialize_output()
+
+        url_prefix = getattr(settings, 'MEDUSA_URL_PREFIX')
+        if url_prefix is not None:
+            set_script_prefix(url_prefix)
 
         for Renderer in get_static_renderers():
             r = Renderer()
